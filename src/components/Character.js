26cec7b446941bb4dd2b951892ca4d17283f188d
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2021-12-10 00:53:36 -0800

    styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React, { useState } from "react";
+import styled from "styled-components";
+// import details from "./Details";
+
+const Container = styled.div`
+  background-color: rgb(0, 0, 0, 0.5);
+  color: white;
+  max-width: 100%;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 3px;
+  margin: 16px;
+  padding: 16, 0, 12, 0;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Div = styled.div`
+  width: 100%;
+`;
